4af2994c4edf3a7e4b67bd7ebd2c04548049bb60
JNN91 <jnngugi6@gmail.com>
2025-08-10 23:10:11 +0300

    user search setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>GitHub User Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
